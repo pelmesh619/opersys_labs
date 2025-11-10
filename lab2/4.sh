0a9787b6aaead458606e692fa6bcce2bca45e316
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-11-10 23:31:10 +0300

    Create 4.sh

---
+++ lab2/4.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+touch 4.log
+
+while read -r line
+do
+	pid=${line%% *}
+	ppid=${line##* }
+	if [[ -f /proc/$pid/sched ]]
+	then
+		runtime=$(cat "/proc/$pid/sched" | grep "sum_exec_runtime" | awk '{ print $3 }')
+		switches=$(cat "/proc/$pid/sched" | grep "nr_switches" | awk '{ print $3 }')
+		echo "ProcessId=$pid : Parent_ProcessID=$ppid : Average_Running_Time=$(echo "scale=3; $runtime / $switches" | bc)" >> 4.log
+	fi
+done < <(ps axo pid,ppid | sort -k2 -n)
